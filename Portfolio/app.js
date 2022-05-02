8d19ce9567d61b7f57a020c8043837e12795d9af
cnh574 <102606659+cnh574@users.noreply.github.com>
2022-05-02 17:42:59 -0400

    Update app.js
    final version

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,40 +1,29 @@
 $(() => {
-  const $h1 = $("<h1>").text("Welcome to my portfolio!").appendTo("body");
+  $("<div>").attr("id", "navbar").appendTo("#container");
+  $("<div>").attr("id", "btn2").text("Resume").appendTo("#navbar");
+  $("#btn2").on("click", () => {
+    location.href = "/Portfolio/Resumeindex.html";
+  });
 });
 
 $(() => {
-  //   const $btn1 = $("<btn1>").attr("id", "btn1");
-  //   console.log($("#btn1"));
+  $("<div>").attr("id", "btn3").text("Bio").appendTo("#navbar");
+  $("#btn3").on("click", () => {
+    location.href = "/Portfolio/bioindex.html";
+  });
+});
+
+$(() => {
+  $("<div>").attr("id", "btn1").text("Projects").appendTo("#navbar");
   $("#btn1").on("click", () => {
-    location.href = "PASTE BIO .HTML";
-
-    $("#btn2").on("click", () => {
-      location.href = "Paste Resume link";
-
-      $("#btn3").on("click", () => {
-        location.href = "Paste Projects link";
-      });
-    });
+    location.href = "/Portfolio/projectindex.html";
   });
 });
 
 $(() => {
-  const $div = $("<div>").attr("id", "popUp").text("Thanks").appendTo("body");
-
-  $("#popUp").on("click", function () {
-    alert(" Thanks for visiting my page and getting to know me! ");
+  $("h1").on({
+    mouseenter: function () {
+      $("h1").css("color", "pink");
+    },
   });
-});
-
-$(() => {
-  $("<div>").attr("id", "ending").appendTo("body");
-  const $footer = $("<footer>").attr("id", "footer").appendTo($("#ending"));
-});
-$(() => {
-  const $ul = $("<ul>").attr("id", "contact").appendTo($("#footer"));
-  $("#contact").append(
-    '<li><a href="https://www.linkedin.com/in/candace-harris-b99921124/">Linkedin</a></li>'
-  );
-  $("#contact").append('<li><a href="#">Instagram</a></li>');
-  $("#contact").append('<li><a href="#">Facebook</a></li>');
 });
